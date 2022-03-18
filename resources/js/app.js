c2421dd1da46a44582f5a67d2e9effb91c15772d
Kristyna <kris@gmail.com>
2022-03-18 10:03:59 +0100

    react connected to laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+    return <div>Hi</div>;
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
